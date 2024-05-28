a2dca3b1c2c7f9077b563d98726cc56c7efe2a91
Heorhi Bazhko <jorkabozko@gmail.com>
2024-05-28 12:43:34 +0200

    add 2 sidebars and routing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Heorhi" };
+  const loggedIn = {
+    firstName: "Heorhi",
+    lastName: "Bazhko",
+    email: "jorkabozko@gmail.com",
+  };
+
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +27,14 @@ const Home = () => {
             totalCurrentBalance={1250.32}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
