c5f782b79de752885471840881039d1e2872acc0
Heorhi Bazhko <jorkabozko@gmail.com>
2024-06-06 11:19:09 +0200

    feat: add auth form

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,17 @@
+"use server";
+
+export const signIn = async () => {
+  try {
+    // Mutation
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signUp = async (userData: SignUpParams) => {
+  try {
+    // Create user account
+  } catch (error) {
+    console.log(error);
+  }
+};
